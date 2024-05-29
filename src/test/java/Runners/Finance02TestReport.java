c96754d98f6a80b5dd811bcdf58a8fed6244251c
MuradHasan0 <workandstudy004@gmail.com>
2024-05-29 23:00:18 +0400

    US009 tamamlandi

---
+++ src/test/java/Runners/Finance02TestReport.java
@@ -0,0 +1,7 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+public class Finance02TestReport extends AbstractTestNGCucumberTests {
+}
