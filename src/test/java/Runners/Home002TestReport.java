c132deb8890b3772283715264afeae1fabcb446f
cilem89 <okkalicilem@gmail.com>
2024-05-29 22:38:01 +0300

    Home002 tamamlandı

---
+++ src/test/java/Runners/Home002TestReport.java
@@ -0,0 +1,13 @@
+package Runners;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features={"src/test/java/FeatureFiles/Home002.feature"}  ,
+        glue={"StepDefinitions"},
+        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
+)
+
+public class Home002TestReport extends AbstractTestNGCucumberTests {
+}
